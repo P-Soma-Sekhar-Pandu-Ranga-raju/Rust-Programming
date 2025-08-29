466c2c86ad3ab41f7711b06c296d4ddb91042338
P Soma Sekhar Pandu Ranga Raju <somupericharla1234@gmail.com>
2025-08-29 10:14:04 +0530

    Create Recursive_Fibonacci_Function.rs

---
+++ Chapter 4/Recursive_Fibonacci_Function.rs
@@ -0,0 +1,14 @@
+fn fibonacci(n: u32) -> u32 {
+    if n == 0 {
+        0
+    } else if n == 1 {
+        1
+    } else {
+        fibonacci(n - 1) + fibonacci(n - 2)
+    }
+}
+
+fn main() {
+    let term = 6;
+    println!("Fibonacci term {} is {}", term, fibonacci(term));
+}
